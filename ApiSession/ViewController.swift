31041d22e4d513247d7627590138c48b6e385ea0
IriSakovich <sakovich@gmx.es>
2025-02-21 23:27:57 +0100

    Initial Commit

---
+++ ApiSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiSession
+//
+//  Created by Irina on 21/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
